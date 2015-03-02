afcd4eeb5bcf06a322a015f57f81edbda840a1d5
alessandroempire <alessandroempire@gmail.com>
2015-03-02 13:03:30 -0430

    agregando la clase para simular fecha actual

---
+++ Rabais/src/rabais/FechaActual.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rabais;
+
+import java.sql.Date; 
+import java.util.Calendar;
+/**
+ *
+ * @author alessandro
+ */
+public class FechaActual {
+    private Date fecha_actual;
+
+    //Constructor
+    public FechaActual(Date fecha_actual) {
+        this.fecha_actual = fecha_actual;
+    }
+    
+    //Getters y setters
+    public Date getFecha_actual() {
+        return fecha_actual;
+    }
+
+    public void setFecha_actual(Date fecha_actual) {
+        this.fecha_actual = fecha_actual;
+    }
+    
+    //Metodo
+    public void nextDay(){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(this.fecha_actual);
+        cal.roll(Calendar.DATE, true);
+        this.fecha_actual = new Date(cal.getTime().getTime());
+    }
+}
