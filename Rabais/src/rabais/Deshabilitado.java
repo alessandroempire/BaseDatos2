f009b402ce5e0fc3eeeb626634e578d75d017be3
Alessandro <alessandroempire@gmail.com>
2015-03-04 20:16:07 -0430

    agregando las clases para el patron state

---
+++ Rabais/src/rabais/Deshabilitado.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rabais;
+
+/**
+  * @Autores:
+ * Alessandro La Corte
+ * Catherine Lollett 
+ */
+public class Deshabilitado implements State{
+    
+}
